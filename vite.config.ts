e8f1f77a5160ea412f7ac3276f4bf751b2b2d150
niksmi <niksmi@rarus.ru>
2025-11-17 07:54:06 +0200

    chore: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/saa-soft-test/',
   plugins: [vue()],
-})
+  server: {
+    port: 3001,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+        chunkFileNames: `assets/[name].js`,
+      },
+    },
+  },
+});
